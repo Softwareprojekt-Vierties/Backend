13c756caeea49b36905dfc35753e3b961ec0bbbf
Angelo Mavridis <Angelo.m1998@hotmail.com>
2024-05-15 00:49:19 +0200

    server  verbindung über localen port getest und test post und get mehtoden geschriben und getestet

--- Server.js
+++ Server.js
@@ -1,16 +1,48 @@
 const express = require("express"); // import express for REST API
 const app = express(); // create app used for the Server 
-const port = 8080; // connection port
+const port = 5000; // connection port
 
 app.use(express.json()); // requiert to parse JSON form requests 
 
-app.get("/test:id", (req,res)=>{
+app.get('/test/:id', (req,res)=>{    // test get function
+   const {id} = req.params;
+   if(id >= 10)
+    {
+       res.status(200).send("lets goo");
+    }
+    else
+    {
+        res.status(200).send("ur id is: ",id);
+    }
+});
+
+app.post('/testpost/:id', (req,res)=>{
     const {id} = req.params;
-    res.send("ur id is" + id);
+    const {servus} = req.body;
+    res.status(200).send("ur id is: "+id+" and ur body is: "+servus);
+});
 
-})
+app.post('/search', (req,res)=>{
+    if(!req.body)
+        {
+            res.status(404).send("no body found");
+        }
+    else
+    {
+        if(req.body.filter == "hallo")
+        {
+            res.status(200).send("you searched for hallo")
+        }
+        else
+        {
+            res.status(400).send("no filter could be found");
+        }
+    }
+    
+});
 
-app.listen(port, (error) => {           // start the server on the port
+
+app.listen(port, (error) => {           // starts the server on the port
     if (error) {
         console.log("Error running the server");
     }
