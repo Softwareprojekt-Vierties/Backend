05f930bfedb3ef3c7ad2ca432e5504892439460e
Angelo Mavridis <Angelo.m1998@hotmail.com>
2024-05-21 19:14:07 +0200

    quickfix

--- Js/Server.js
+++ Js/Server.js
@@ -15,8 +15,8 @@ app.use(cors(corsOption))
 app.use(express.json()); // requiert to parse JSON form requests 
 app.use(cookieParser()); // requiert to parse cookies
 app.use((req, res, next) => {
-    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('X-Content-Type-Options', 'nosniff');
     next();
   });
 
