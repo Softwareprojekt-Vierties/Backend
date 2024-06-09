07282389382a38c98c851856a1c85cb9bf7113e3
Angelo Mavridis <Angelo.m1998@hotmail.com>
2024-06-09 18:54:58 +0200

    == in registration geändert

--- Js/Registration.js
+++ Js/Registration.js
@@ -4,6 +4,6 @@ module.exports = async (req, res) => {
 
     const {email,pass,name} = req.body;
     await Database.createEndUser(name,"test",email,pass,null,null,null,null,18,null,null,null,null).then(result => {
-        if(result=true) {return res.status(200).send("User created")}
+        if(result==true) {return res.status(200).send("User created")}
         else{ return res.status(400).send("User not created")} });
 }
