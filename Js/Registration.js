cc5652b0c12c4fee22a77f596bf63e5de5b4257d
JustCallMeKK <kirill.kuhn@gmx.de>
2024-06-09 20:59:57 +0200

    removed 'test' from username

--- Js/Registration.js
+++ Js/Registration.js
@@ -1,9 +1,8 @@
 const Database = require('./Database.js');
 
 module.exports = async (req, res) => {
-
     const {email,pass,name} = req.body;
-    await Database.createEndUser(name,"test",email,pass,null,null,null,null,18,null,null,null,null).then(result => {
+    await Database.createEndUser(name,"User_"+name,email,pass,null,null,null,null,18,null,null,null,null).then(result => {
         if(result==true) {return res.status(200).send("User created")}
         else{ return res.status(400).send("User not created")} });
 }
