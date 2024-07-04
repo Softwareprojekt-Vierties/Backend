db3a6285718d61ca829c2559adb9947e537ce284
JustCallMeKK <kirill.kuhn@gmx.de>
2024-07-04 20:13:31 +0200

    updated register

--- Js/Registration.js
+++ Js/Registration.js
@@ -10,6 +10,7 @@ const CreateQueries = require('./Database/CreateQueries');
 module.exports = async (req, res) => {
     const {email,pass,name} = req.body;
     await CreateQueries.createEndUser(name,"User_"+name,email,pass,null,null,null,null,18,null,null,null,null).then(result => {
-        if(result==true) {return res.status(200).send("User created")}
-        else{ return res.status(400).send("User not created")} });
+        if(result.success) {return res.status(200).send("User created")}
+        else{ return res.status(500).send("User not created: " + result.error)} 
+    });
 }
