a46b08cfd88f12adb2ef0fac8bca0cc58b3fa290
JustCallMeKK <kirill.kuhn@gmx.de>
2024-07-07 00:21:04 +0200

    debug

--- Js/Database/GetQueries.js
+++ Js/Database/GetQueries.js
@@ -789,7 +789,7 @@ async function getCatererById(req,res){
         )
 
         return res.status(200).send({
-            isOwner : userid === cater.rows[0]['id'] ? true : false,
+            isOwner : userid === cater.rows[0]['userid'] ? true : false,
             caterer: cater,
             gerichte: gericht,
             events : event
@@ -855,7 +855,7 @@ async function getArtistByID(req,res){
         )
 
         return res.status(200).send({
-            isOwner : userid === art.rows[0]['id'] ? true : false,
+            isOwner : userid === art.rows[0]['userid'] ? true : false,
             artist: art,
             lieder: lied,
             events : event
@@ -934,8 +934,10 @@ async function getEndUserById(req,res){
             [id]
         )
 
+        console.log(user)
+
         return res.status(200).send({
-            isMe : userid === user.rows[0]['id'] ? true : false,
+            isMe : userid === user.rows[0]['userid'] ? true : false,
             user : user,
             locations : location,
             owenevents : owenevent,
