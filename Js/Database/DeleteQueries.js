83d69e3df58b0c60155913217e8822f6b5d1e579
Angelo Mavridis <Angelo.m1998@hotmail.com>
2024-07-09 17:03:31 +0200

    fixed typo bei delete bild

--- Js/Database/DeleteQueries.js
+++ Js/Database/DeleteQueries.js
@@ -748,7 +748,7 @@ async function deleteBildById(id) {
     try {
         console.warn("TRYING TO DELETE A bild OF", id)
         const result = await pool.query(
-            `DELETE FROM bild WHERE id = %1::int RETURNING *`,
+            `DELETE FROM bild WHERE id = $1::int RETURNING *`,
             [id]
         )
         if (result.rows.length === 0) {
